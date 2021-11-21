2e40e86898d75d56369ba7f2bb34ec35d2db4829
SubramanivelC <83161956+SubramanivelC@users.noreply.github.com>
2021-11-21 22:51:42 +0530

    Update Expense.js

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -8,7 +8,7 @@ import Card from '../UI/Card'
 
   const Expense = (props) => {
 
-    const [filteredYear, setFilteredYear] = useState('2020')
+    const [filteredYear, setFilteredYear] = useState('2021')
 
     const GetYearChangedValueHandler = (selectedYear) =>{
       setFilteredYear(selectedYear);
